a9a854c5ecb8b79fbc21c3a7083bf7c6f1276b22
wanglei <806416060@qq.com>
2024-05-28 09:39:40 +0800

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>home</div>
+</template>
+
+<script setup lang="ts">
+import { registerSW } from 'virtual:pwa-register'
+
+onMounted(() => {
+  registerSW({
+    immediate: true,
+    onRegisteredSW(_url, registration) {
+      console.log('🚀 ~ onRegisteredSW ~ registration:', registration)
+      console.log('🚀 ~ onRegisteredSW ~ _url:', _url)
+      setInterval(() => {
+        registration && registration.update()
+      }, 3600000)
+    }
+  })
+})
+</script>
+
+<style scoped></style>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
